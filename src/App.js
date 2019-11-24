06dce6c9db3614c0107802ab1b8c4dd6780341f6
Brijeshlakkad <brijeshlakkad22@gmail.com>
2019-11-24 16:55:01 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello'
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className="App">
       <Hello />
+      <Welcome />
     </div>
   );
 }
