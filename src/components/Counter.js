76479413ab713bcddf7daf9997d9f083679ab46a
Brijeshlakkad <brijeshlakkad22@gmail.com>
2019-11-26 09:50:11 +0530

    react groups multiple setState call to one call

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,30 +2,50 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 class Counter extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      count: 0
-    }
-  }
-  increment(){
-    this.setState({
-      count: this.state.count + 1
-    },
-      () => {
-        console.log("Callback value: ", this.state.count)
+    constructor() {
+      super();
+      this.state = {
+        count: 0
       }
-    );
-    console.log(this.state.count);  // This is because call to setState is asynchronous
-  }
-  render () {
-    return (
-      <div>
-        <h1>Count - {this.state.count} </h1>
-        <button onClick={()=> this.increment()}>Click</button>
-      </div>
-    )
-  }
-}
+    }
+    increment() {
+      // this.setState({
+      //   count: this.state.count + 1
+      // },
+      //   () => {
+      //     console.log("Callback value: ", this.state.count)
+      //   }
+      // );
+      this.setState(prevState => ({
+        count: prevState.count + 1
+        }), () => {
+              console.log("Callback value: ", this.state.count)
+            }
+        );
+        console.log(this.state.count); // This is because call to setState is asynchronous
+      }
 
-export default Counter;
+      incrementFive() {
+        //This is because react groups multiple setState call to one call
+        this.increment();
+        this.increment();
+        this.increment();
+        this.increment();
+        this.increment();
+      }
+      render() {
+        return ( <
+          div >
+          <
+          h1 > Count - {
+            this.state.count
+          } < /h1> <
+          button onClick = {
+            () => this.incrementFive()
+          } > Click < /button> <
+          /div>
+        )
+      }
+    }
+
+    export default Counter;
