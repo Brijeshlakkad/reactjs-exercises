26b4bb55febdb4084aabc7fc59e8548d2a004b65
Brijeshlakkad <brijeshlakkad22@gmail.com>
2019-11-26 09:24:02 +0530

    state example done

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import './App.css';
 import Hello from './components/Hello'
 import Welcome from './components/Welcome'
 import Greet from './components/Greet'
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
+     <Message />
       { /* <Hello /> */}
-      <Greet name="Brijesh" age="21">
+      {
+      /*  <Greet name="Brijesh" age="21">
         Description: Software Developer
       </Greet>
       <Greet name="XYZ" age="22" />
       <Welcome name="Brijesh" age="22">
         Description: Software Developer
       </Welcome>
-      <Welcome name="XYZ" age="21" />
+      <Welcome name="XYZ" age="21" />  */
+      }
     </div>
   );
 }
