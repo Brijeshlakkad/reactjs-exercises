b5611fa71d628b0aaa131ac80033f446c1c469b0
Brijeshlakkad <brijeshlakkad22@gmail.com>
2019-11-30 19:43:29 +0530

    used ref using React.createRef and using ref callback function

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import EventBind from "./components/EventBind";
 import ParentComponent from "./components/ParentComponent";
 import NameList from "./components/NameList";
 import Form from "./components/Form";
+import RefsDemo from "./components/RefsDemo";
 
 function App() {
   return (
     <div className="App">
       {/*<ParentComponent />*/}
       {/*<NameList/>*/}
-      <Form/>
+      {/*<Form/>*/}
+      <RefsDemo/>
     </div>
   );
 }
---
+++ src/components/RefsDemo.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+
+class RefsDemo extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state={
+
+        };
+        this.inputRef  = new React.createRef();
+        this.clickHandler = this.clickHandler.bind(this);
+
+        this.cbRef = null;
+        this.setCbRef = element =>{
+            this.cbRef = element;
+        }
+    }
+
+    componentDidMount() {
+        this.cbRef.focus();
+
+        // this.inputRef.current.focus();
+        // console.log(this.inputRef)
+    }
+
+    clickHandler(){
+        alert(`${this.inputRef.current.value}`)
+    }
+
+    render() {
+        return (
+            <div>
+                {/*<input type='text' ref={this.inputRef}/>*/}
+                <input type='text' ref={this.setCbRef}/>
+                <button onClick={this.clickHandler}>Click</button>
+            </div>
+        );
+    }
+}
+
+export default RefsDemo;
