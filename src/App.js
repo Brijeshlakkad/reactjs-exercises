ebc8551a8fe4be65f9cf7617e2d1980b441eac35
Brijeshlakkad <brijeshlakkad22@gmail.com>
2019-11-30 18:58:54 +0530

    list rendering with key

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import EventBind from "./components/EventBind";
 import ParentComponent from "./components/ParentComponent";
+import NameList from "./components/NameList";
 
 function App() {
   return (
     <div className="App">
-      <ParentComponent />
+      {/*<ParentComponent />*/}
+      <NameList/>
     </div>
   );
 }
---
+++ src/components/NameList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NameList(){
+    const names = ['Bruce', 'Clark', 'Diana'];
+    return (
+        <div>
+            {
+                names.map((name, index) => <h2 key={index}>{name}</h2>)
+            }
+        </div>
+    );
+}
+
+export default NameList;
