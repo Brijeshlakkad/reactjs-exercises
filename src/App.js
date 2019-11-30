d209ef61283d1325126321cb259a12ecfb416767
Brijeshlakkad <brijeshlakkad22@gmail.com>
2019-11-30 20:21:16 +0530

    portal-root element created and using ReactDOM.createPortal added JSX of PortalDemo into portal-root element

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Form from "./components/Form";
 import RefsDemo from "./components/RefsDemo";
 import FocusInput from "./components/FocusInput";
 import FRParentInput from "./components/FRParentInput";
+import PortalDemo from "./components/PortalDemo";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
       {/*<Form/>*/}
       {/*<RefsDemo/>*/}
       {/*<FocusInput/>*/}
-      <FRParentInput />
+      {/*<FRParentInput />*/}
+      <PortalDemo/>
     </div>
   );
 }
