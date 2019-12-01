4d3737175ad7968603c4e9f833db9634a928e772
Brijeshlakkad <brijeshlakkad22@gmail.com>
2019-12-01 14:52:17 +0530

    4. Form with useState using object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import HookCounter from "./components/HookCounter";
+import HookCounterThree from "./components/HookCounterThree";
 
 function App() {
   return (
     <div className="App">
-      <HookCounter/>
+      <HookCounterThree/>
     </div>
   );
 }
---
+++ src/components/HookCounterThree.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+
+function HookCounterThree(){
+    const [name, setName] = useState({firstName: '', lastName: ''})
+    return (
+        <div>
+            <form>
+                <input type='text'
+                       value={name.firstName}
+                       onChange={(e) => setName({...name, firstName: e.target.value})} />
+                <input type='text'
+                       value={name.lastName}
+                       onChange={(e) => setName({...name, lastName: e.target.value})} />
+            </form>
+            <div>{JSON.stringify(name)}</div>
+        </div>
+    )
+}
+
+export default HookCounterThree
